bb93dab92d766389dad362bf1851dc0cf21a1423
luanrem <luanrem@gmail.com>
2023-08-19 17:50:17 -0300

    C02 - Widget de Espaço Usado

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,11 @@
-import { Search } from 'lucide-react'
+import { BarChart, CheckSquare, Cog, Flag, HomeIcon, LifeBuoy, Search, SquareStack, Users } from 'lucide-react'
 import { Logo } from "./Logo";
-import { MainNavigation } from './MainNavigation';
+import { NavItem } from './NavItem';
+import { UsedSpaceWidget } from './UsedSpaceWidget';
 
 
 export function Sidebar() {
-  return <aside className='space-y-6 border-r border-zinc-200 px-5 py-8'>
+  return <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
     <Logo />
 
     <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -12,6 +13,15 @@ export function Sidebar() {
       <input className='flex-1 border-0 outline-none bg-transparent p-0 text-zinc-900 placeholder-zinc-300' placeholder='Search' />
     </div>
 
-    <MainNavigation />
+    <nav className="space-y-0.5">
+      <NavItem title='Home' icon={HomeIcon} />
+      <NavItem title='Dashboard' icon={BarChart} />
+      <NavItem title='Projects' icon={SquareStack} />
+      <NavItem title='Tasks' icon={CheckSquare} />
+      <NavItem title='Reporting' icon={Flag} />
+      <NavItem title='Users' icon={Users} />
+    </nav>
+
+    <UsedSpaceWidget />
   </aside>
 }
