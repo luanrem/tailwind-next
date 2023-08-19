4dbdc0b7b93addba7adcaabdc3312c6ae01307bd
luanrem <luanrem@gmail.com>
2023-08-19 19:57:27 -0300

    C03 - Campos do Formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Mail } from "lucide-react";
+import { Input } from "./components/Input";
 import { SettingsTabs } from "./components/SettingsTabs";
 
 export default function Home() {
@@ -30,7 +32,105 @@ export default function Home() {
           </div>
         </div>
 
-        <form id="settings" action="" className="mt-6 flex flex-col w-full">form</form>
+        <form id="settings" action="" className="mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-200">
+          <div className="grid gap-3 grid-cols-form">
+            <label htmlFor="firstName" className="text-sm font-medium text-zinc-700">Name</label>
+            <div className="grid gap-6 grid-cols-2">
+              <Input.Root>
+                <Input.Control id="firstName" defaultValue="Luan" />
+              </Input.Root>
+
+              <Input.Root>
+                <Input.Control defaultValue="Roberto" />
+              </Input.Root>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="email" className="text-sm font-medium text-zinc-700">Email address</label>
+            <Input.Root>
+              <Input.Prefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </Input.Prefix>
+              <Input.Control id="email" type="email" defaultValue="luanrem@gmail.com" />
+            </Input.Root>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="mt-0.5 text-sm font-normal text-zinc-500 block">This will be displayed on your profile.</span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">Role</label>
+            <Input.Root>
+              <Input.Control id="role" type="text" defaultValue="CTO" />
+            </Input.Root>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="country" className="text-sm font-medium text-zinc-700">Country</label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="timezone" className="text-sm font-medium text-zinc-700">Timezone</label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="bio"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Bio
+              <span
+                className="mt-0.5 text-sm font-normal text-zinc-500 block"
+              >
+                Write a short introduction.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Portfolio projects
+              <span
+                className="mt-0.5 text-sm font-normal text-zinc-500 block"
+              >
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              form="settings"
+              type="submit"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+
+        </form>
       </div>
     </>
   )
