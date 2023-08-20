81343b199e7821a4dd3d043dc93e420d596674fc
luanrem <luanrem@gmail.com>
2023-08-20 15:36:56 -0300

    C05 - Variante ghost dos Botões

--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from "lucide-react";
+import { Button } from "../Button";
 
 
 export function Profile() {
@@ -13,9 +14,9 @@ export function Profile() {
         <span className="text-sm font-semibold text-zinc-700">Luan Martins</span>
         <span className="truncate text-sm text-zinc-500">luanrem@gmail.com</span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
