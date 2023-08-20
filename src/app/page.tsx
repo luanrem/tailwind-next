729127672497426021d967619a4baf44618bce8e
luanrem <luanrem@gmail.com>
2023-08-20 13:43:25 -0300

    C03 - Upload do Avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Mail } from "lucide-react";
+import { Mail, UploadCloud, User } from "lucide-react";
 import { Input } from "./components/Input";
 import { SettingsTabs } from "./components/SettingsTabs";
 
@@ -64,7 +64,33 @@ export default function Home() {
               Your photo
               <span className="mt-0.5 text-sm font-normal text-zinc-500 block">This will be displayed on your profile.</span>
             </label>
-            <div></div>
+            <div className="flex items-start gap-5">
+              <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <User className="w-8 h-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+              <input type="file" className="sr-only" id="photo" />
+            </div>
           </div>
 
           <div className="grid gap-3 grid-cols-form pt-5">
